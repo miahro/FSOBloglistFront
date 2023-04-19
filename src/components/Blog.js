0ec08eea98dc2f94b255b3bbc3f6693f2364e7eb
miahro <mikko.ahro@gmail.com>
2023-04-19 16:24:31 +0300

    Ex 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,51 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+
+
+const Blog = ({blog}) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    paddingBottom: 10,
+    border: 'solid',
+    borderColor: 'blue',
+    borderWidth: 2,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const like = () => {
+    console.log("Like clicked")
+  }
+
+
+  return (
+  <div style={blogStyle}>
+
+      <div style={hideWhenVisible}>
+        {blog.title} &nbsp;
+        <button onClick={toggleVisibility}> view </button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} &nbsp;
+        <button onClick={toggleVisibility}>hide</button> <br></br>
+        {blog.author} <br></br>
+        {blog.url} <br></br>
+        likes {blog.likes} &nbsp;
+        <button onClick={like} >like</button><br></br>
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
